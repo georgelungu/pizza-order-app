73fa65314efd4558feb5992752e159a8775fe0a9
georgelungu <george.lungu1896@gmail.com>
2023-06-26 22:50:37 +0300

    switched-the-pc

--- backend/server.js
+++ backend/server.js
@@ -10,35 +10,57 @@ const allergensPath = path.join(`${__dirname}/allergens.json`);
 
 // console.log(pizzaPath + "\n" + allergensPath)
 
-
 const app = express();
 const port = 9002;
 
-app.use(express.static(`${__dirname}/../frontend/public`))
+// Function to get all the URLs called by getting each page.
+app.use((req, res, next) =>
+{
+    console.log(req.url)
+    next()
+})
+
+// Middlewares for using & processing the data.
+app.use(express.static(__dirname + "/../frontend/public"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Getting all the pizzas in JSON format.
 app.get('/api/pizzas', async (req, res) =>
 {
     console.log(req.url)
     let data = await fileReaderAsync(pizzaPath)
-    parsedData = JSON.parse(data)
+    let parsedData = JSON.parse(data)
     let pizzas = parsedData.pizzas
     res.send(pizzas)
 })
 
+// Getting all the allergens in JSON format.
 app.get('/api/allergens', async (req, res) =>
 {
     console.log(req.url)
     let data = await fileReaderAsync(allergensPath)
-    parsedData = JSON.parse(data)
+    let parsedData = JSON.parse(data)
     let allergens = parsedData.allergens
     res.send(allergens)
 })
 
-app.get('/pizza/list', (req, res) =>
+// Create a list of pizzas that contain ingredients and allergens.
+app.get('/', async (req, res) =>
 {
-    res.sendFile('D:/VS CODE/CODECOOL/WEB FRAMEWORKS/pizza-order-prototype-javascript-georgelungu/frontend/index.html')
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'))
+})
+
+app.get('/pizza/list', async (req, res) =>
+{
+    console.log(req.url)
+    let pizzaData = await fileReaderAsync(pizzaPath)
+    let parsedPizzaData = JSON.parse(pizzaData)
+    let pizzas = parsedPizzaData.pizzas
+
+    let allergensData = await fileReaderAsync(allergensPath)
+    let parsedAllergensData = JSON.parse(allergensData)
+    let allergens = parsedAllergensData.pizzas
 })
 
 app.listen(port, _ => console.log(`http://127.0.0.1:${port}`));
---
+++ frontend/public/script.js
@@ -0,0 +1,12 @@
+    // works
+// let root = document.getElementById('root')
+// let frontPage = '/pizza-shop-images/front-page.jpg'
+// console.log('hello')
+// let image = new Image()
+// image.src = frontPage
+// let firstImage = document.getElementById('myImage') // assuming that there is an img element in the html
+// firstImage.src = image.src
+
+
+
+
